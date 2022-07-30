77e6c6df92eb50c2138195b9fc15f7f90b151c49
Joe Shabadu <joeshabadu2000@yahoo.com>
2022-07-30 11:45:36 -0400

    1

--- Zabbix/zabbixsetup.ps1
+++ Zabbix/zabbixsetup.ps1
@@ -12,4 +12,4 @@ Set-AzContext $subscriptionName
 New-AzResourceGroup -Name $rgName -Location $projectLocation
 
 # Deploy Script using variables listed above
-New-AzResourceGroupDeployment -ResourceGroupName $rgName -TemplateFile "https://github.com/JoeShabadu2000/AzurePublic/blob/4db9de4c47d2a29f75fb08c24c0275db65b056a2/Zabbix/zabbixsetup.bicep" -projectName $projectName -projectLocation $projectLocation -vmName $vmName
+New-AzResourceGroupDeployment -ResourceGroupName $rgName -TemplateFile "./zabbixsetup.bicep" -projectName $projectName -projectLocation $projectLocation -vmName $vmName
