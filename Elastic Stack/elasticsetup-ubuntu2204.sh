e5d6498aac250480d00cbd384e28fa5e8e52b5f9
Joe Shabadu <joeshabadu2000@yahoo.com>
2022-08-02 12:16:51 -0400

    1

---
+++ Elastic Stack/elasticsetup-ubuntu2204.sh
@@ -0,0 +1,126 @@
+### Run this file inside the VM after it has been provisioned in Azure
+### wget -O - https://raw.githubusercontent.com/JoeShabadu2000/AzurePublic/main/Zabbix/zabbixsetup-ubuntu2204.sh | bash
+
+#####Variables#########
+
+managed_identity_id=$1
+time_zone=$2
+swap_file_size=$3
+keyvault_name=$4
+mysql_root_password=
+mysql_zabbix_password=
+letsencrypt_email=
+letsencrypt_domain=
+
+#######General#############
+
+# Open the following ports in Azure: 22, 80, 443, 10050, 10051
+
+# Set Time Zone
+
+sudo timedatectl set-timezone $time_zone
+
+# Set up swap file and enable
+
+sudo fallocate -l $swap_file_size /swapfile && sudo chmod 600 /swapfile && sudo mkswap /swapfile && sudo swapon /swapfile
+
+# Use crontab to add the swap file to reenable at reboot by adding the following line
+
+echo "@reboot azureuser sudo fallocate -l $swap_file_size /swapfile && sudo chmod 600 /swapfile && sudo mkswap /swapfile && sudo swapon /swapfile" | sudo tee -a /etc/crontab
+
+# Change Ubuntu needrestart behavior so that it does not restart daemons, so as to not freeze up the setup script
+
+sudo sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'l'"'"';/g' /etc/needrestart/needrestart.conf
+
+# Install Azure CLI
+
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+# Login to Azure using the VM's user assigned managed identity
+
+az login --identity -u $managed_identity_id
+
+# Pull secrets from Azure Keyvault (the sed section is to strip first and last characters (quotes) from the JSON output)
+
+mysql_root_password=$(az keyvault secret show --name mysql-root-password --vault-name $keyvault_name --query "value" | sed -e 's/^.//' -e 's/.$//')
+
+mysql_zabbix_password=$(az keyvault secret show --name mysql-zabbix-password --vault-name $keyvault_name --query "value" | sed -e 's/^.//' -e 's/.$//')
+
+letsencrypt_email=$(az keyvault secret show --name letsencrypt-email --vault-name $keyvault_name --query "value" | sed -e 's/^.//' -e 's/.$//')
+
+letsencrypt_domain=$(az keyvault secret show --name letsencrypt-domain --vault-name $keyvault_name --query "value" | sed -e 's/^.//' -e 's/.$//')
+
+# Install VIM & Curl & Midnight Commander & Rsync
+
+sudo apt-get update && sudo apt-get upgrade -y
+
+sudo apt-get install vim curl mc rsync -y
+
+# To change VIM color scheme settings
+
+echo "colorscheme desert" | sudo tee -a /etc/vim/vimrc
+
+#######Install Zabbix#######
+
+# Install Zabbix Repo
+
+sudo wget https://repo.zabbix.com/zabbix/6.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.0-3+ubuntu22.04_all.deb && sudo dpkg -i zabbix-release_6.0-3+ubuntu22.04_all.deb && sudo apt update
+
+# Install Zabbix server, frontend, agent
+
+sudo apt-get install zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent -y
+
+# Install mySQL server
+
+sudo apt-get install mysql-server -y
+
+# Edit MySQL config file to disable binary logging (to prevent log files from getting too large)
+
+echo -e "[mysqld]\n\nskip-log-bin" | sudo tee -a /etc/init/mysql.conf
+
+# Set root password to mySQL
+
+sudo mysqladmin -u root password $mysql_root_password
+
+# Create database named zabbix, user zabbix, and password specified at start of script
+
+sudo mysql -uroot -p$mysql_root_password -e "create database zabbix character set utf8mb4 collate utf8mb4_bin;create user zabbix@localhost identified by '$mysql_zabbix_password';grant all privileges on zabbix.* to zabbix@localhost;SET GLOBAL log_bin_trust_function_creators = 1;"
+
+# Import schema into Zabbix database (may appear to hang, be patient)
+
+sudo zcat /usr/share/doc/zabbix-sql-scripts/mysql/server.sql.gz | mysql -uzabbix -p$mysql_zabbix_password zabbix
+
+# Copy database password into zabbix config file
+
+sudo sed -i "s/# DBPassword=/DBPassword=$mysql_zabbix_password/g" "/etc/zabbix/zabbix_server.conf"
+
+# Start Zabbix server and agent processes
+
+sudo systemctl restart zabbix-server zabbix-agent apache2
+
+sudo systemctl enable zabbix-server zabbix-agent apache2
+
+# Install Let's Encrypt certificate for frontend
+
+# sudo apt-get install certbot python3-certbot-apache -y
+
+# sudo certbot --apache -m $letsencrypt_email --agree-tos --non-interactive -d $letsencrypt_domain
+
+# Point Apache directly to /usr/share/zabbix so that your FQDN takes you
+# directly to the Zabbix interface
+
+# sudo sed -i 's#DocumentRoot /var/www/html#DocumentRoot /usr/share/zabbix#g' /etc/apache2/sites-available/000-default-le-ssl.conf
+
+# sudo systemctl restart apache2
+
+# You should be able to log in to web interface by going to your FQDN
+# Default u: Admin  p: zabbix
+
+
+# To Backup MySql database and config files every night
+
+sudo mkdir /backup/ && sudo chmod 777 /backup/
+
+echo '00 02 * * * azureuser sudo mysqldump --no-tablespaces -uzabbix -p'$mysql_zabbix_password' zabbix | sudo gzip -c > /backup/ZabbixSQLBackup.`date +\%a`.sql.gz' | sudo tee -a /etc/crontab
+
+echo '00 02 * * * azureuser sudo tar -zcf /backup/ZabbixConfigBackup.`date +\%a`.tar.gz -C /etc/zabbix/ .' | sudo tee -a /etc/crontab
---
+++ Elastic Stack/elasticsetup.ps1
@@ -0,0 +1,94 @@
+# Setup script for Zabbix on Azure
+#
+# Installs all Zabbix files and MySQL server on the same VM for ease of setup
+# Also uses certbot to generate HTTPS certificate w/ auto renewal through Letsencrypt
+# Should just be able to run this script, then visit https://yoururl.azure.com/ to complete setup
+# 
+# What does it do?
+# 
+# - Creates a new resource group in Azure
+#
+# - Uses Powershell to deploy a Bicep template that creates:
+#   - Vnet and Subnet
+#   - Public IP Address
+#   - Network Security Group
+#   - NIC
+#   - VM running Ubuntu 22.04 w/ Custom Script Extension for Linux
+#
+# - VM Bash setup script:
+#   - Bash script is hosted on Github public repo, contains no secrets
+#   - Bicep template specifies function in Custom Script Extension to execute shell command to pull script from Github and execute it in Bash using variables passed in from Powershell
+#   - Logs in to Azure AD using the user managed identity string (passed through Bicep as a Bash variable)
+#   - Pulls secrets from Azure Keyvault to use as passwords for MySQL and Letsencrypt registration info
+#   - Installs Zabbix and MySQL server from repos
+#   - Uses certbot to generate HTTPS certificate w/ auto renewal through Letsencrypt
+#   - Sets up misc items like page file, time zone, cron jobs for backups and to handle reboots
+#
+#
+# Defaults to a B1s instance with 1 CPU and 1G RAM
+#
+# This script assumes you have:
+# - The SSH Key for console login pre-generated, with the public key stored in a separate resource group in the same subscription
+# - A "zabbix" user managed identity created in in a separate resource group in the same subscription
+# - An Azure Keyvault that:
+#    - Is set for Azure RBAC access (under Settings -> Access Policies)
+#    - Has the "zabbix" user managed identity assigned the Key Vault Secrets User role on the Keyvault (Access control IAM)
+#    - Has 4 secrets pre-populated in the keyvault
+#      - mysql-root-password
+#      - mysql-zabbix-password
+#      - letsencrypt-email
+#      - letsencrypt-domain
+
+
+#############
+# Variables #
+#############
+
+# General Variables
+$projectName = "elastic"
+$projectLocation = "eastus"
+$subscriptionName = "Microsoft Partner Network"
+$rgName = "rg-$projectName"
+
+# VM Creation Variables
+$vmName = "TA1-SV16-Elastic"
+$sshkeyRgName = "rg-keyvault"  ## Name of the resource group that you store your SSH keys in
+$sshkeyName = "vmkey"          ## Name of the public SSH key you want to use for the VM
+$managedidentityResourceGroup = "rg-usermanagedidentities"
+$managedidentityName = "elastic"
+
+# VM Setup Script Variables (passed into Bash using Custom Script Extension)
+$vmSetupScriptURL = "https://raw.githubusercontent.com/JoeShabadu2000/AzurePublic/main/Zabbix/zabbixsetup-ubuntu2204.sh"
+$vmTimeZone = "America/New_York"
+$vmSwapFileSize = "4G"
+$vmKeyVaultName = "keyvault-zabbix"
+
+##################
+# Start of Setup #
+##################
+
+# Set Correct Subscription
+Set-AzContext $subscriptionName
+
+# Create Resource Group
+New-AzResourceGroup -Name $rgName -Location $projectLocation
+
+# Get SSH Public Key from Existing Resource Group, to use when setting up VM
+$sshkey = Get-AzSshKey -ResourceGroupName $sshkeyRgName -Name $sshkeyName
+
+# Get Managed Identity ID, allows for login to Azure AD to access Keyvault
+$managedidentityID = Get-AzUserAssignedIdentity -Name $managedidentityName -ResourceGroupName $managedidentityResourceGroup
+
+# Deploy Bicep template using variables listed above
+New-AzResourceGroupDeployment `
+    -ResourceGroupName $rgName `
+    -TemplateFile "./zabbixsetup.bicep" `
+    -projectName $projectName `
+    -projectLocation $projectLocation `
+    -vmName $vmName `
+    -sshpublickey $sshkey.publicKey `
+    -vmSetupScriptURL $vmSetupScriptURL `
+    -managedidentityID $managedidentityID.Id `
+    -vmTimeZone $vmTimeZone `
+    -vmSwapFileSize $vmSwapFileSize `
+    -vmKeyVaultName $vmKeyVaultName
