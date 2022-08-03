e1344d7f752e8b0e2f5cbdfaf142059589b5cac6
Joe Shabadu <joeshabadu2000@yahoo.com>
2022-08-03 09:33:17 -0400

    1

--- Elastic/elasticsetup-ubuntu2204.sh
+++ Elastic/elasticsetup-ubuntu2204.sh
@@ -101,7 +101,7 @@ sudo systemctl enable kibana && sudo systemctl start kibana
 
 # Download IPTables, forward port 80 to 5601 for Kibana GUI
 
-sudo apt install iptables
+# sudo apt install iptables
 
 # sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 5601
 
@@ -114,13 +114,13 @@ sudo apt install iptables
 
 # Install Caddy & set to reverse proxy HTTPS to Kibana
 
-sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
-sudo apt update
-sudo apt install caddy
-sudo caddy reverse-proxy --from tabulaelastic.eastus.cloudapp.azure.com --to 127.0.0.1:5601
-echo "sudo caddy reverse-proxy --from tabulaelastic.eastus.cloudapp.azure.com --to 127.0.0.1:5601" | sudo tee -a /etc/crontab
+# sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
+# curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+# curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+# sudo apt update
+# sudo apt install caddy
+# sudo caddy reverse-proxy --from tabulaelastic.eastus.cloudapp.azure.com --to 127.0.0.1:5601
+# echo "sudo caddy reverse-proxy --from tabulaelastic.eastus.cloudapp.azure.com --to 127.0.0.1:5601" | sudo tee -a /etc/crontab
 
 # To Backup MySql database and config files every night
 
