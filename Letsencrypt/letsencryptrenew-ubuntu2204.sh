450f08bff06311842bfeaf82b2fd25f8615f0bcc
Joe Shabadu <joeshabadu2000@yahoo.com>
2022-08-06 22:14:03 -0400

    1

---
+++ Letsencrypt/letsencryptrenew-ubuntu2204.sh
@@ -0,0 +1,100 @@
+### Run this file inside the VM after it has been provisioned in Azure
+### wget -O - https://raw.githubusercontent.com/JoeShabadu2000/AzurePublic/main/Elastic/letsencryptsetup-ubuntu2204.sh | bash
+
+#####Variables#########
+
+managed_identity_id=$1
+time_zone=$2
+swap_file_size=$3
+keyvault_name=$4
+ssl_cert_name=$5
+
+#######General#############
+
+# Open the following ports in Azure: 22, 80, 443
+
+# Set Time Zone
+
+sudo timedatectl set-timezone $time_zone
+
+# Set up swap file and enable
+
+sudo fallocate -l $swap_file_size /swapfile && sudo chmod 600 /swapfile && sudo mkswap /swapfile && sudo swapon /swapfile
+
+# Use crontab to add the swap file to reenable at reboot by adding the following line
+
+echo "@reboot azureuser sudo fallocate -l $swap_file_size /swapfile && sudo chmod 600 /swapfile && sudo mkswap /swapfile && sudo swapon /swapfile" | sudo tee -a /etc/crontab
+
+# Change Ubuntu needrestart behavior so that it does not restart daemons, so as to not freeze up the setup script
+
+sudo sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'l'"'"';/g' /etc/needrestart/needrestart.conf
+
+# Install Azure CLI
+
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+# Login to Azure using the VM's user assigned managed identity
+
+az login --identity -u $managed_identity_id
+
+# Write managed identity id & ssl cert name to the system profile so it becomes an available variable for future logins for any user
+
+echo "export managed_identity_id=$managed_identity_id
+export ssl_cert_name=$ssl_cert_name
+export keyvault_name=$keyvault_name" | sudo tee -a /etc/profile
+
+# Edit .bashrc for azureuser so that it logs in to the managed identity any time the user is logged in
+
+echo "az login --identity -u $managed_identity_id" | sudo tee -a /home/azureuser/.bashrc
+
+# Pull secrets from Azure Keyvault (the sed section is to strip first and last characters (quotes) from the JSON output)
+
+FQDN=$(az keyvault secret show --name FQDN --vault-name $keyvault_name --query "value" --output tsv)
+
+# mysql_zabbix_password=$(az keyvault secret show --name mysql-zabbix-password --vault-name $keyvault_name --query "value" | sed -e 's/^.//' -e 's/.$//')
+
+letsencrypt_email=$(az keyvault secret show --name letsencrypt-email --vault-name $keyvault_name --query "value" --output tsv)
+
+# letsencrypt_domain=$(az keyvault secret show --name letsencrypt-domain --vault-name $keyvault_name --query "value" | sed -e 's/^.//' -e 's/.$//')
+
+# Install VIM & Curl & Midnight Commander & Rsync
+
+sudo apt-get update && sudo apt-get upgrade -y
+
+sudo apt-get install vim curl mc rsync -y
+
+# To change VIM color scheme settings
+
+echo "colorscheme desert" | sudo tee -a /etc/vim/vimrc
+
+#######################
+# Generate Cert & Key #
+#######################
+
+# Generate Certificate Request through Keyvault
+
+# az keyvault certificate create --vault-name $keyvault_name --name $ssl_cert_name --policy '{"x509CertificateProperties": {"subject":"CN='$FQDN'"},"issuerParameters": {"name": "Unknown"}}'
+
+# Retrieve CSR file that needs to be sent to certificate authority
+
+# az keyvault certificate pending show --vault-name $keyvault_name --name $ssl_cert_name --query csr -o tsv | sudo tee ./cert.csr
+
+# Modify CSR File for Letsencrypt
+
+# sed -i '1 s/^/-----BEGIN CERTIFICATE REQUEST-----\n/' ./cert.csr
+
+# echo "-----END CERTIFICATE REQUEST-----" | sudo tee -a ./cert.csr
+
+# Install and run certbot to obtain certificates
+
+sudo apt-get install certbot -y
+
+# sudo certbot certonly --standalone --agree-tos -n -m $letsencrypt_email --csr ./cert.csr -d $FQDN
+
+# Upload full certificate to keyvault
+
+# az keyvault certificate pending merge --vault-name $keyvault_name --name $ssl_cert_name --file ./0001_chain.pem
+
+# To delete keys in Keyvault
+# az keyvault certificate delete --vault-name $keyvault_name --name $ssl_cert_name
+# az keyvault certificate purge --vault-name $keyvault_name --name $ssl_cert_name
---
+++ Letsencrypt/letsencryptrenew.ps1
@@ -0,0 +1,75 @@
+# Will renew an exsting SSL cert stored in an Azure Keyvault through Let's Encrypt, using an automated DNS challenge
+#
+# Defaults to a B1s instance with 1 CPU and 1G RAM
+#
+# This script assumes you have:
+# - The SSH Key for console login pre-generated, with the public key stored in a separate resource group in the same subscription
+# - A "letsencrypt" user managed identity created in in a separate resource group in the same subscription
+# - An Azure Keyvault that:
+#    - Is set for Azure RBAC access (under Settings -> Access Policies)
+#    - Has the "letsencrypt" user managed identity assigned the Key Vault Adminstrator role on the Keyvault (Access control IAM)
+#    - Has 2 secrets pre-populated in the keyvault
+#      - letsencrypt-email
+#      - FQDN
+# - DNS records properly pointed to the FQDN you have specified
+#
+# JSON schema for certificates can be found with:
+# az keyvault certificate get-default-policy --scaffold
+
+
+#############
+# Variables #
+#############
+
+# General Variables
+$projectName = "letsencrypt"
+$projectLocation = "eastus"
+$subscriptionName = "Microsoft Partner Network"
+$rgName = "rg-$projectName"
+
+# VM Creation Variables
+$vmName = "TA1-SV99-Letsencrypt"
+$sshkeyRgName = "rg-keyvault"  ## Name of the resource group that you store your SSH keys in
+$sshkeyName = "vmkey"          ## Name of the public SSH key you want to use for the VM
+$managedidentityResourceGroup = "rg-usermanagedidentities"
+$managedidentityName = "letsencrypt"
+$publicIPDomainName = "letsencrypt227"  ## DNS name for public IP (will concatenate [name].[region].cloudapp.azure.com) - must be global unique
+
+# VM Setup Script Variables (passed into Bash using Custom Script Extension)
+$vmSetupScriptURL = "https://raw.githubusercontent.com/JoeShabadu2000/AzurePublic/main/Letsencrypt/letsencryptrenew-ubuntu2204.sh"
+$vmTimeZone = "America/New_York"
+$vmSwapFileSize = "1G"
+$vmKeyVaultName = "keyvault-elastic"
+$sslCertName = "sslcert-elastic"
+
+##################
+# Start of Setup #
+##################
+
+# Set Correct Subscription
+Set-AzContext $subscriptionName
+
+# Create Resource Group
+New-AzResourceGroup -Name $rgName -Location $projectLocation
+
+# Get SSH Public Key from Existing Resource Group, to use when setting up VM
+$sshkey = Get-AzSshKey -ResourceGroupName $sshkeyRgName -Name $sshkeyName
+
+# Get Managed Identity ID, allows for login to Azure AD to access Keyvault
+$managedidentityID = Get-AzUserAssignedIdentity -Name $managedidentityName -ResourceGroupName $managedidentityResourceGroup
+
+# Deploy Bicep template using variables listed above
+New-AzResourceGroupDeployment `
+    -ResourceGroupName $rgName `
+    -TemplateFile "./letsencryptrenew.bicep" `
+    -projectName $projectName `
+    -projectLocation $projectLocation `
+    -vmName $vmName `
+    -sshpublickey $sshkey.publicKey `
+    -vmSetupScriptURL $vmSetupScriptURL `
+    -managedidentityID $managedidentityID.Id `
+    -vmTimeZone $vmTimeZone `
+    -vmSwapFileSize $vmSwapFileSize `
+    -vmKeyVaultName $vmKeyVaultName `
+    -sslCertName $sslCertName `
+    -publicIPDomainName $publicIPDomainName
