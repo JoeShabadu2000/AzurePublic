a624b0ec99d4dc6d8ade37b350419d01a5b1eead
Joe Shabadu <joeshabadu2000@yahoo.com>
2022-08-07 14:39:46 -0400

    1

---
+++ Letsencrypt/letsencryptrenew2-ubuntu2204.sh
@@ -0,0 +1,62 @@
+### Run this file inside the VM after it has been provisioned in Azure
+### wget -O - https://raw.githubusercontent.com/JoeShabadu2000/AzurePublic/main/Elastic/letsencryptsetup-ubuntu2204.sh | bash
+
+#####Variables#########
+
+managed_identity_id=$1
+time_zone=$2
+swap_file_size=$3
+keyvault_name=$4
+ssl_cert_name=$5
+managed_identity_clientid=$6
+dns_rg_id=$7
+
+#######General#############
+
+# Open the following ports in Azure: 22, 80, 443
+
+# Set Time Zone
+
+sudo timedatectl set-timezone $time_zone
+
+# Set up swap file and enable
+
+sudo fallocate -l $swap_file_size /swapfile && sudo chmod 600 /swapfile && sudo mkswap /swapfile && sudo swapon /swapfile
+
+# Use crontab to add the swap file to reenable at reboot by adding the following line
+
+echo "@reboot azureuser sudo fallocate -l $swap_file_size /swapfile && sudo chmod 600 /swapfile && sudo mkswap /swapfile && sudo swapon /swapfile" | sudo tee -a /etc/crontab
+
+# Change Ubuntu needrestart behavior so that it does not restart daemons, so as to not freeze up the setup script
+
+sudo sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'l'"'"';/g' /etc/needrestart/needrestart.conf
+
+# Install Azure CLI
+
+curl https://azurecliprod.blob.core.windows.net/install | bash
+
+hash -r
+
+# Login to Azure using the VM's user assigned managed identity
+
+az login --identity -u $managed_identity_id
+
+# Write managed identity id & ssl cert name to the system profile so it becomes an available variable for future logins for any user
+
+echo "export managed_identity_id=$managed_identity_id
+export ssl_cert_name=$ssl_cert_name
+export keyvault_name=$keyvault_name
+export managed_identity_clientid=$managed_identity_clientid
+export dns_rg_id=$dns_rg_id" | sudo tee -a /etc/profile
+
+# Edit .bashrc for azureuser so that it logs in to the managed identity any time the user is logged in
+
+echo "az login --identity -u $managed_identity_id" | sudo tee -a /home/azureuser/.bashrc
+
+# Pull secrets from Azure Keyvault (the sed section is to strip first and last characters (quotes) from the JSON output)
+
+FQDN=$(az keyvault secret show --name FQDN --vault-name $keyvault_name --query "value" --output tsv)
+
+letsencrypt_email=$(az keyvault secret show --name letsencrypt-email --vault-name $keyvault_name --query "value" --output tsv)
+
+echo "$FQDN" | sudo tee /user/azureuser/test.txt
