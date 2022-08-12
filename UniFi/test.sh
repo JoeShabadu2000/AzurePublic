265f87f6e300b62549820ef6b0c4a1e9fee62525
Joe Shabadu <joeshabadu2000@yahoo.com>
2022-08-12 12:49:47 -0400

    1

--- UniFi/test.sh
+++ UniFi/test.sh
@@ -2,4 +2,4 @@ managed_identity_clientid=$1
 time_zone=$2
 keyvault_name=$3
 
-curl https://azurecliprod.blob.core.windows.net/install | bash
+curl https://azurecliprod.blob.core.windows.net/install | sh
